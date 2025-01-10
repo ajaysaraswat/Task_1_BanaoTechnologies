413522ede19edccd88186a5fec06dfaad3dde1fe
AJAY KUMAR SARASWAT <ajaykumarsaraswat9045@gmail.com>
2025-01-10 19:02:01 +0530

    user creation and password hashing

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,8 +1,23 @@
+const User = require("../models/user");
+
 const handlegetuser = (req, res) => {
 	return res.status(200).send({ message: "successful get" });
 };
 
-const handlepostuser = (req, res) => {};
+const handlepostuser = (req, res) => {
+	try {
+		const body = req.body;
+		if (!body) return res.status(400).send({ message: "invalid body" });
+		User.create({
+			fullname: body.fullname,
+			email: body.email,
+			password: body.password,
+		});
+		console.log("user created");
+	} catch (err) {
+		throw console.error("Error", err);
+	}
+};
 
 module.exports = {
 	handlegetuser,
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -24,5 +24,18 @@ const userSchema = mongoose.Schema(
 	{ timestamps: true }
 );
 
+userSchema.pre("save", function (next) {
+	const user = this;
+	if (!user.isModified("password")) return;
+	const salt = randomBytes(16).toString();
+	const hashedPassword = createHmac("sha256", salt)
+		.update(user.password)
+		.digest("hex");
+	this.salt = salt;
+	this.password = hashedPassword;
+
+	next();
+});
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
